49c0c531423411bc53daa85845cdd66afd2f2af1
Diptipathak2007 <diptipathak019@gmail.com>
2025-06-18 17:07:18 +0000

    technically built my first smart contract

---
+++ simplestorage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.18;//stating our version of solidity
+
+contract simplestorage{
+    //basic types:boolean,uint,int,address,bytes
+    bool hasfavoritenumber= true;
+    uint favoritenumber=88;
+}
